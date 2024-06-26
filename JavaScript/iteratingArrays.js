544713be5658efa43bafe6be127cc5faaee78423
Islinger19 <Sanidhya.awasthi@gmail.com>
2024-06-26 14:23:11 +0530

    JavaScript

---
+++ JavaScript/iteratingArrays.js
@@ -0,0 +1,60 @@
+// ===================================
+// ITERATING OVER ARRAYS!
+// ===================================
+
+const animals = [
+    "Aardvark",
+    "Aardwolf",
+    "African Elephant",
+    "African Tree Pangolin",
+    "Alligator",
+    "Alpaca",
+    "Anteater",
+    "Antelope",
+    "Ape(s)",
+    "Arabian Horse",
+    "Armadillo",
+    "Arthropod(s)",
+    "Asian Elephant",
+    "Aye-Aye",
+    "Black Bear",
+    "Black Footed Rhino",
+    "Black Howler Monkey",
+    "Black Rhino",
+    "Blacktip Reef Shark",
+    "Blue Shark",
+    "Blue Whale",
+    "Boar",
+    "Bob-Cat",
+    "Bonobo",
+    "Walrus",
+    "Warthog",
+    "Water Buffalo",
+    "Weasel",
+    "Whale Shark",
+    "Whale(s)",
+    "White Rhino",
+    "White Tailed Deer",
+    "White-Beaked Dolphin",
+    "Whitetip Reef Shark",
+    "Wildcat",
+    "Wildebeest",
+    "Wobbegong Shark",
+    "Wolf",
+    "Wolverine",
+    "Wombat",
+    "Woodchuck",
+    "Yak",
+    "Yellow-Bellied Marmot",
+    "Zebra",
+    "Zebu",
+    "Zorilla"
+]
+
+for (let i = 0; i < animals.length; i++) {
+    console.log(i, animals[i])
+}
+
+// for (let i = animals.length - 1; i >= 0; i--) {
+//     console.log(animals[i])
+// }
