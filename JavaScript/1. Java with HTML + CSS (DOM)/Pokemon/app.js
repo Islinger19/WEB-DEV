f79247ba527ac9e6c3e100c5834470c9d3769068
Islinger19 <Sanidhya.awasthi@gmail.com>
2024-06-28 16:08:00 +0530

    JavaScript

---
+++ JavaScript/1. Java with HTML + CSS (DOM)/Pokemon/app.js
@@ -0,0 +1,18 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+const container = document.querySelector('#container');
+
+
+for (let i = 1; i <= 151; i++) {
+    const newDiv = document.createElement('div');
+    const newSpan = document.createElement('span');
+    newSpan.innerText = `#${i}`;
+    const newImg = document.createElement('img');
+    newImg.src = `
+https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`
+
+
+    newDiv.appendChild(newImg);
+    newDiv.appendChild(newSpan);
+    container.appendChild(newDiv)
+}
